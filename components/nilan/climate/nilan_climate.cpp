83fcee9f1749a4eaed19113817640c9262dab8eb
dyrvigk <dyrvig.br@gmail.com>
2023-12-26 12:51:00 +0100

    Update nilan_climate.cpp

--- components/nilan/climate/nilan_climate.cpp
+++ components/nilan/climate/nilan_climate.cpp
@@ -95,8 +95,8 @@ climate::ClimateTraits NilanClimate::traits() {
   });
 
   traits.set_supported_modes({
-    climate::ClimateMode::CLIMATE_MODE_HOTWATER,
     climate::ClimateMode::CLIMATE_MODE_HEAT,
+    climate::ClimateMode::CLIMATE_MODE_COOL,
     climate::ClimateMode::CLIMATE_MODE_HEAT_COOL
    });
 
